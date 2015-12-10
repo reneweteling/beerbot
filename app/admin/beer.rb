7eabb62257470ca8c34816d6e7cc35d0cc4983ff
Rene Weteling <rene@weteling.com>
2015-12-10 19:56:49 +0100

    Metrics

--- app/admin/beer.rb
+++ app/admin/beer.rb
@@ -1,2 +1,17 @@
 ActiveAdmin.register Beer do
+
+  index do
+    selectable_column
+    id_column
+    column :amount
+    column :user, sortable: :user_id do |col|
+      col.user.to_s
+    end
+    column :date, sortable: :created_at  do |col|
+      col.created_at.to_date
+    end
+    column :created_at
+    actions
+  end
+
 end
