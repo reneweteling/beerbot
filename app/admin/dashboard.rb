388787089366173b6db9a2f8337233c74557b486
Rene Weteling <rene@weteling.com>
2015-12-11 08:44:53 +0100

    Better stats

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span "Beers consumed: #{Beer.consumed.sum(:amount) * -1} bought: #{Beer.bought.sum(:amount)}"
-        h2 "There should be #{Beer.sum(:amount)} in stock"
-      end
-    end
-
+    render partial: 'stats'
   end
 end
