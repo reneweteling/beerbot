d0d65ee006cd5ae1b7080b3071f55f3f0a730ae4
Rene Weteling <rene@weteling.com>
2015-12-11 08:46:49 +0100

    Forgot some file

---
+++ app/lib/stats.rb
@@ -0,0 +1,37 @@
+class Stats
+  def self.user
+
+    beer_by_date = Beer.bought.group("DATE(created_at), user_id")
+      .where("created_at > ?", 30.days.ago)
+      .select("DATE(created_at) as date", :user_id, "SUM(amount) as amount")
+      .order("DATE(created_at) asc")
+
+    dates = {}
+    beer_by_date.each do |b|
+      if dates[b.date].present?
+        dates[b.date].push(b) 
+      else
+        dates[b.date] = [b]
+      end
+    end
+
+    users = User.order(:first_name).where('beer_total > ?', 0)
+    data = [ ['Datum'] + users.collect{|u| u.first_name } ]
+    dates.each do |date, beer|
+      row = [ date.strftime("%Y-%m-%d") ]
+      beer_users = beer.collect{|b| b.user_id }
+      users.each do |user|
+        index = beer_users.index user.id
+        unless index.nil?
+          row << beer[index].amount
+          next
+        end
+        row << 0
+      end
+      data << row
+    end
+
+    data
+
+  end
+end
