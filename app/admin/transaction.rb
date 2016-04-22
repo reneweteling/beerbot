80f740e7be67217c3f949dcc5ed10eeb723a1379
Rene Weteling <rene@weteling.com>
2016-04-22 08:54:27 +0200

    Added user to transaction page

--- app/admin/transaction.rb
+++ app/admin/transaction.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Transaction do
 
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :money
+    column :amount
+    column :paid
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :user
