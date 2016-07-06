ef8df442b5d8b7bc4a6b931b716a6b8c00b03aa6
Mehul Gurjar <gurjar.mehul@gmail.com>
2016-07-06 23:49:10 +0530

    Issue#4: Remove the transaction table / model, active admin trasaction resoource.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331062840) do
+ActiveRecord::Schema.define(version: 20160706180527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,17 +40,6 @@ ActiveRecord::Schema.define(version: 20160331062840) do
 
   add_index "beers", ["user_id"], name: "index_beers_on_user_id", using: :btree
 
-  create_table "transactions", force: :cascade do |t|
-    t.integer  "user_id",                    null: false
-    t.float    "money",      default: 0.0,   null: false
-    t.integer  "amount",     default: 0,     null: false
-    t.boolean  "paid",       default: false, null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -70,5 +59,4 @@ ActiveRecord::Schema.define(version: 20160331062840) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "beers", "users"
-  add_foreign_key "transactions", "users"
 end
